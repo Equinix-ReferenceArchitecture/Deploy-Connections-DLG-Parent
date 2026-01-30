1645db986da8ba716a1369e51a7ed51c76b3335c
vipin2060 <vipinvikraman86@gmail.com>
2026-01-31 00:29:05 +0800

    Add Terraform module for Deploy-Redundant-DLG

---
+++ main.tf
@@ -0,0 +1,21 @@
+module "Deploy-Redundant-DLG-Parent" {
+  source = "github.com/Equinix-ReferenceArchitecture/Deploy-Child-Redundant-DLG.git"
+
+equinix_client_id     = var.equinix_client_id
+equinix_client_secret = var.equinix_client_secret
+account_number = var.account_number
+project_id = var.project_id
+Pri-DLG_name = var.Pri-DLG_name
+Sec-DLG_name = var.Sec-DLG_name
+PriDLG_InterfacenumberNE1 = var.PriDLG_InterfacenumberNE1
+PriDLG_InterfacenumberNE2 = var.PriDLG_InterfacenumberNE2
+PriDLG-bandwidth-NE1 = var.PriDLG-bandwidth-NE1
+PriDLG-bandwidth-NE2 = var.PriDLG-bandwidth-NE2
+NE1-Metro = var.NE1-Metro
+NE2-Metro = var.NE2-Metro
+SecDLG_InterfacenumberNE1 = var.SecDLG_InterfacenumberNE1
+SecDLG_InterfacenumberNE2 = var.SecDLG_InterfacenumberNE2
+SecDLG-bandwidth-NE1 = var.SecDLG-bandwidth-NE1
+SecDLG-bandwidth-NE2 = var.SecDLG-bandwidth-NE2
+
+}
