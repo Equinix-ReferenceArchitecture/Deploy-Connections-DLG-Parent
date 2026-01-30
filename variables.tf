67ce328ab2227aeb351f1497464bd585da9db3a9
vipin2060 <vipinvikraman86@gmail.com>
2026-01-31 00:29:47 +0800

    Add variables for DLG configuration in Terraform

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "Pri-DLG_name" {
+  description = "Name of Primary DLG"
+  type        = string
+}
+
+variable "Sec-DLG_name" {
+  description = "Name of Secondary DLG"
+  type        = string
+}
+
+variable "project_id" {
+description = "Project ID" 
+ type        = string
+}
+
+variable "PriDLG_InterfacenumberNE1" {
+description = "Primary Interface number for DLG on NE1" 
+ type        = string
+}
+
+variable "PriDLG_InterfacenumberNE2" {
+description = "Primary Interface number for DLG on NE2" 
+ type        = string
+}
+
+variable "PriDLG-bandwidth-NE1" {
+description = "bandwidth speed for Primary DLG on NE1" 
+ type        = string
+}
+
+variable "PriDLG-bandwidth-NE2" {
+description = "bandwidth speed for Primary DLG on NE2" 
+ type        = string
+}
+
+variable "NE1-Metro" {
+description = "Metro for NE1" 
+ type        = string
+}
+
+variable "NE2-Metro" {
+description = "Yes Metro for NE2" 
+ type        = string
+}
+
+variable "SecDLG_InterfacenumberNE1" {
+description = "Secondary Interface number for DLG on NE1" 
+ type        = string
+}
+
+variable "SecDLG_InterfacenumberNE2" {
+description = "Secondary Interface number for DLG on NE2" 
+ type        = string
+}
+
+variable "SecDLG-bandwidth-NE1" {
+description = "bandwidth speed for Secondary DLG on NE1" 
+ type        = string
+}
+
+variable "SecDLG-bandwidth-NE2" {
+description = "bandwidth speed for Secondary DLG on NE2" 
+ type        = string
+}
+
+variable "account_number" {
+  description = "Account Number for the first device" 
+  type        = number
+}
